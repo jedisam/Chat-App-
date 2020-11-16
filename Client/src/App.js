032b8994a0f1757cb27964707d388bf78c216fa2
Yididya Samuel <yidisam18@gmail.com>
2020-11-16 12:40:18 -0500

    better styling

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,14 +3,14 @@ import {BrowserRouter as Router, Route} from "react-router-dom"
 
 import Join from "./Components/Join/Join"
 import Chat from "./Components/Chat/Chat"
-import Home from "./Components/Home/Home"
+// import Home from "./Components/Home/Home"
 
 export default class App extends React.Component{
     render(){
         return(
             <Router>
-            <Route  path="/" exact component={Home}/>
-            <Route  path="/join" component={Join}/> 
+            <Route  path="/" exact component={Join}/>
+            {/* <Route  path="/join" component={Join}/>  */}
             <Route path="/chat" component={Chat}/>
          </Router>
         )
